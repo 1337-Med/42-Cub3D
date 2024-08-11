03db03fbd2eaa50e57c7db904c7f8c8e3378e3a1
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-08-11 16:04:49 +0100

    ray_angle

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: amejdoub <amejdoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 13:05:54 by nbenyahy          #+#    #+#             */
-/*   Updated: 2024/08/11 15:26:35 by amejdoub         ###   ########.fr       */
+/*   Updated: 2024/08/11 16:00:03 by amejdoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "libft.h"
 # include "../mlx/include/MLX42/MLX42.h"
 #define PI 3.14159265358979323846
+#define NUM_RAYS 320
 int parser(int ac, char **av);
 
 # endif
--- sources/raycasting/utils.c
+++ sources/raycasting/utils.c
@@ -6,7 +6,7 @@
 /*   By: amejdoub <amejdoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 15:25:01 by amejdoub          #+#    #+#             */
-/*   Updated: 2024/08/11 15:26:53 by amejdoub         ###   ########.fr       */
+/*   Updated: 2024/08/11 16:01:43 by amejdoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,10 @@
 int degree_to_raidian(int dg)
 {
     return (dg * (PI / 180));
+}
+
+int ray_angle()
+{
+    int fov = degree_to_raidian(60);
+    return (fov / NUM_RAYS);
 }
