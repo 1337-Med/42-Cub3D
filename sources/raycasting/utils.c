1f04bd69490da7a349cc71cb506071947dda8177
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-08-11 15:28:04 +0100

    dg to rd function

--- sources/raycasting/utils.c
+++ sources/raycasting/utils.c
@@ -1,21 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amejdoub <amejdoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/10 13:05:54 by nbenyahy          #+#    #+#             */
-/*   Updated: 2024/08/11 15:26:35 by amejdoub         ###   ########.fr       */
+/*   Created: 2024/08/11 15:25:01 by amejdoub          #+#    #+#             */
+/*   Updated: 2024/08/11 15:26:53 by amejdoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
+#include "cub3d.h"
 
-# include "libft.h"
-# include "../mlx/include/MLX42/MLX42.h"
-#define PI 3.14159265358979323846
-int parser(int ac, char **av);
-
-# endif
+int degree_to_raidian(int dg)
+{
+    return (dg * (PI / 180));
+}
