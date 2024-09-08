07614be2208f0b89692b0434c56b4a28bd0e34a1
nourddine-benyahya <nourddine.benyahya02@gmail.com>
2024-09-08 15:50:06 +0100

    norm the mouse

--- sources/cub3d.c
+++ sources/cub3d.c
@@ -3,23 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amejdoub <amejdoub@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nbenyahy <nbenyahy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 13:07:11 by nbenyahy          #+#    #+#             */
-/*   Updated: 2024/09/03 13:04:32 by amejdoub         ###   ########.fr       */
+/*   Updated: 2024/09/07 17:21:08 by nbenyahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void leaks(void)
+int	main(int ac, char **av)
 {
-	system("leaks cub3d");
-}
-
-int main(int ac, char **av)
-{
-	// atexit(leaks);
 	raycaster(parser(ac, av));
 	return (0);
 }
--- sources/raycasting/mouse_rotation.c
+++ sources/raycasting/mouse_rotation.c
@@ -6,32 +6,32 @@
 /*   By: nbenyahy <nbenyahy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 15:36:55 by amejdoub          #+#    #+#             */
-/*   Updated: 2024/09/07 17:26:14 by nbenyahy         ###   ########.fr       */
+/*   Updated: 2024/09/08 15:49:25 by nbenyahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void ft_loop(void *data)
+void	ft_loop(void *data)
 {
-    static int last_x = WIDTH / 2;
-	static int i = 0;
-    int current_x = WIDTH / 2, current_y = HEIGHT / 2;
-    int screen_center_x = WIDTH / 2;
-    int screen_center_y = HEIGHT / 2;
+	static int	i = 0;
+	int			current_x;
+	int			current_y;
+	int			delta_x;
 
+	current_x = WIDTH / 2;
+	current_y = HEIGHT / 2;
 	if (i > 1)
-    	mlx_get_mouse_pos(((t_shared_data *)data)->mlx, &current_x, &current_y);
-    int delta_x = current_x - last_x;
-    if (delta_x != 0)
-    {
-        ((t_shared_data *)data)->player.rota_angle += delta_x * 0.01;
-		((t_shared_data *)data)->player.rota_angle = norm_angle(((t_shared_data *)data)->player.rota_angle);
-    }
-    last_x = current_x;
-    mlx_set_mouse_pos(((t_shared_data *)data)->mlx, screen_center_x, screen_center_y);
-    last_x = screen_center_x;
-    render_map(data);
+		mlx_get_mouse_pos(((t_shared_data *)data)->mlx, &current_x, &current_y);
+	delta_x = current_x - WIDTH / 2;
+	if (delta_x != 0)
+	{
+		((t_shared_data *)data)->player.rota_angle += delta_x * 0.01;
+		((t_shared_data *)data)->player.rota_angle = \
+		norm_angle(((t_shared_data *)data)->player.rota_angle);
+	}
+	mlx_set_mouse_pos(((t_shared_data *)data)->mlx, WIDTH / 2, HEIGHT / 2);
+	render_map(data);
 	if (i < 2)
 		i++;
 }
