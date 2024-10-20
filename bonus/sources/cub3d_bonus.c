ee3913b36d77a79269bc3a0c5788562262ae8baa
nourddine-benyahya <nourddine.benyahya02@gmail.com>
2024-10-20 11:38:21 +0100

    up

--- bonus/sources/cub3d_bonus.c
+++ bonus/sources/cub3d_bonus.c
@@ -3,22 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d_bonus.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amejdoub <amejdoub@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nbenyahy <nbenyahy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 13:07:11 by nbenyahy          #+#    #+#             */
-/*   Updated: 2024/10/12 12:08:56 by amejdoub         ###   ########.fr       */
+/*   Updated: 2024/10/20 11:35:41 by nbenyahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 
-void ll()
-{
-	system("leaks -q cub3D_bonus");
-}
 int	main(int ac, char **av)
 {
-	atexit(ll);
 	raycaster(parser(ac, av));
 	return (0);
 }
