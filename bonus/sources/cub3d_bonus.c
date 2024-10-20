2940c76e4b4574ca0d4627707092540846452cac
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-10-20 11:33:18 +0100

    norm

--- bonus/sources/cub3d_bonus.c
+++ bonus/sources/cub3d_bonus.c
@@ -6,19 +6,14 @@
 /*   By: amejdoub <amejdoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 13:07:11 by nbenyahy          #+#    #+#             */
-/*   Updated: 2024/10/12 12:08:56 by amejdoub         ###   ########.fr       */
+/*   Updated: 2024/10/20 11:31:04 by amejdoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 
-void ll()
-{
-	system("leaks -q cub3D_bonus");
-}
 int	main(int ac, char **av)
 {
-	atexit(ll);
 	raycaster(parser(ac, av));
 	return (0);
 }
