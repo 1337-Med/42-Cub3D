692f6c571b19357d9d596a8e05f3b371ed00670b
nourddine-benyahya <nourddine.benyahya02@gmail.com>
2024-11-01 15:27:54 +0100

    remove some shity stuffs

--- bonus/sources/cub3d_bonus.c
+++ bonus/sources/cub3d_bonus.c
@@ -6,20 +6,14 @@
 /*   By: nbenyahy <nbenyahy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 10:52:51 by amejdoub          #+#    #+#             */
-/*   Updated: 2024/11/01 12:21:15 by nbenyahy         ###   ########.fr       */
+/*   Updated: 2024/11/01 15:25:59 by nbenyahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 
-void ll()
-{
-	system("leaks -q cub3D_bonus");
-}
-
 int	main(int ac, char **av)
 {
-	// atexit(ll);
 	raycaster(parser(ac, av));
 	return (0);
 }
--- mandatory/sources/cub3d.c
+++ mandatory/sources/cub3d.c
@@ -6,20 +6,14 @@
 /*   By: nbenyahy <nbenyahy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 13:07:11 by nbenyahy          #+#    #+#             */
-/*   Updated: 2024/11/01 11:30:37 by nbenyahy         ###   ########.fr       */
+/*   Updated: 2024/11/01 15:26:16 by nbenyahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void ll()
-{
-	system("leaks -q cub3D");
-}
-
 int	main(int ac, char **av)
 {
-	// atexit(ll);
 	raycaster(parser(ac, av));
 	return (0);
 }
